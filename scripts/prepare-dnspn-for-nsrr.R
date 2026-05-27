315dcd67d89c42f3a282cb69128fdeb7c1b6ab47
Isabella Liu <isabellaliu0617@gmail.com>
2026-05-27 14:54:10 -0400

    staging

--- scripts/prepare-dnspn-for-nsrr.R
+++ scripts/prepare-dnspn-for-nsrr.R
@@ -8,7 +8,7 @@ data <- data %>% rename_with(tolower)
 data <- data %>%
   mutate(visit = 0) %>%
   relocate(visit, .after = subjectid)
-write.csv(data, "/VOLUMES/BWH-SLEEPEPI-NSRR-STAGING/20260310-maski-dns/nsrr-prep/0.1.0.pre/dnspn-dataset.0.1.0.pre.csv", row.names = FALSE, na = '')
+write.csv(data, "/VOLUMES/BWH-SLEEPEPI-NSRR-STAGING/20260310-maski-dns/nsrr-prep/0.1.0.pre/dnspn-dataset-0.1.0.pre.csv", row.names = FALSE, na = '')
 
 # harmonized dataset
 harmonized_data <- data[,c("subjectid","visit","age","race","gender")]%>%
@@ -29,22 +29,21 @@ harmonized_data <- data[,c("subjectid","visit","age","race","gender")]%>%
   select(nsrrid,visit,nsrr_age,nsrr_race,nsrr_sex)
 
 psg_variables <- data %>%
-  select(psgtib, psg_tstmin, psg_waso, psg_se, psg_sol, psg_remlat, n1perc, n2perc, n3perc, remperc, rem_duration, arousalindex, obahi) %>%
+  select(psgtib, psg_tstmin, psg_waso, psg_se, psg_sol, n1perc, n2perc, n3perc, remperc, rem_duration, arousalindex, obahi) %>%
   rename(nsrr_tib_f1=psgtib,
          nsrr_tst_f1=psg_tstmin,
          nsrr_waso_f1=psg_waso,
          nsrr_ttleffsp_f1=psg_se,
          nsrr_ttllatsp_f1=psg_sol,
-         nsrr_ttlprdsp_s1sr=psg_remlat,
          nsrr_pctdursp_s1=n1perc,
          nsrr_pctdursp_s2=n2perc,
          nsrr_pctdursp_s3=n3perc,
          nsrr_pctdursp_sr=remperc,
          nsrr_ttldursp_sr=rem_duration,
          nsrr_phrnumar_f1=arousalindex,
-         nsrr_oahi=obahi
+         nsrr_ahi_hp3r_aasm07=obahi
   )
 
 harmonized_data <- bind_cols(harmonized_data, psg_variables)
 
-write.csv(harmonized_data, "/VOLUMES/BWH-SLEEPEPI-NSRR-STAGING/20260310-maski-dns/nsrr-prep/0.1.0.pre/dnspn-harmonized-dataset.0.1.0.pre.csv", row.names = FALSE, na = '')
+write.csv(harmonized_data, "/VOLUMES/BWH-SLEEPEPI-NSRR-STAGING/20260310-maski-dns/nsrr-prep/0.1.0.pre/dnspn-harmonized-dataset-0.1.0.pre.csv", row.names = FALSE, na = '')
